98bcf3947e6c3ccb8f59b0072dbcb1f786caf829
Anh Tran <quocanh.bk09@gmail.com>
2016-05-25 11:17:00 +0700

    First Hello World. the book is outdated.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	entry: [
+		'./source/App.js'
+	],
+
+	output: {
+		path: __dirname,
+		filename: "bundle.js"
+	},
+
+	module: {
+		loaders:[{
+			test: /\.jsx?$/,
+			loader: 'babel',
+			query:
+		      {
+		        presets:['es2015', 'react']
+		      }
+		}]
+	}
+};
